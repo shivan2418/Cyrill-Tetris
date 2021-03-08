7cdc3e89c0ad653ef0266edef0796087389fd02c
Emil Elgaard <shivan@shivan.dk>
2021-03-08 13:31:17 -0500

    refactor menus

--- src/js/classes/game.js
+++ src/js/classes/game.js
@@ -1,266 +1,266 @@
-const { LetterBlock } = require("./letter_block.js");
-const { RUS_ALL_LETTERS_FULL,RUS_ALL_LETTERS_HARD_ONLY, RU_TO_LAT_FULL } = require("../alphabet.js");
-const { XCOLS, COL_WIDTH, ROW_HEIGHT, YROWS } = require('../settings.js');
-const { setCookie, getRandomInt, random_choice } = require('./utility');
-const {NEW_BLOCK_DELAY, BLOCK_SPEED,HINT_DELAY,HINT_FADEIN,HINT_FADEOUT} = require('../settings.js');
+const {LetterBlock} = require("./letter_block.js");
+const {RUS_ALL_LETTERS_FULL, RUS_ALL_LETTERS_HARD_ONLY, RU_TO_LAT_FULL} = require("../alphabet.js");
+const {XCOLS, COL_WIDTH, ROW_HEIGHT, YROWS} = require('../settings.js');
+const {setCookie, getRandomInt, random_choice} = require('./utility');
+const {NEW_BLOCK_DELAY, BLOCK_SPEED, HINT_DELAY, HINT_FADEIN, HINT_FADEOUT} = require('../settings.js');
 
 class Game {
 
-    constructor(){
+    constructor() {
 
-      // state
-      this.running = false;
-      this.show_hints=true;
-      this.current_mode = 'endless';
-      this.current_exam = null;
-      
-      this.endless_points = 0;
+        // state
+        this.running = false;
+        this.show_hints = true;
+        this.current_mode = 'endless';
+        this.current_exam = null;
+        this.challenge_mode = false;
 
-      this.block_speed = BLOCK_SPEED;
-      this.last_block_added_time = null;
-      this.last_blocked_added_in_round = 0;     
-      this.letters_on_screen=0;
-      this.blocks = [];
-    
-      //exam
-      this.exam_progress={};
+        this.endless_points = 0;
 
-      //syllabus
-      this.syllabus = RUS_ALL_LETTERS_FULL;
+        this.block_speed = BLOCK_SPEED;
+        this.last_block_added_time = null;
+        this.last_blocked_added_in_round = 0;
+        this.letters_on_screen = 0;
+        this.blocks = [];
 
-      // Constants
-      this.MAX_BLOCKS_ON_SCREEN = 5; // max blocks at once on screen
-      this.NEW_BLOCK_DELAY = NEW_BLOCK_DELAY; // second delay between adding new blocsk
-    
-      this.shown_blocks_endless = {}
+        //exam
+        this.exam_progress = {};
+
+        //syllabus
+        this.syllabus = RUS_ALL_LETTERS_FULL;
+
+        // Constants
+        this.MAX_BLOCKS_ON_SCREEN = 5; // max blocks at once on screen
+        this.NEW_BLOCK_DELAY = NEW_BLOCK_DELAY; // second delay between adding new block
+
+        this.shown_blocks_endless = {}
 
     }
 
 
-  show_wrong_answer_hint(letter) {
-    if(this.show_hints){
-      //Show a hint with the jamo and the reading. 
-      $("#wrong_answer_hint").text(`${letter} : ${RU_TO_LAT_FULL[letter]}`);
-      $("#wrong_answer_hint").fadeIn(HINT_FADEIN).delay(HINT_DELAY).fadeOut(HINT_FADEOUT);
+    show_wrong_answer_hint(letter) {
+        if (this.show_hints) {
+            //Show a hint with the jamo and the reading.
+            $("#wrong_answer_hint").text(`${letter} : ${RU_TO_LAT_FULL[letter]}`);
+            $("#wrong_answer_hint").fadeIn(HINT_FADEIN).delay(HINT_DELAY).fadeOut(HINT_FADEOUT);
+        }
     }
-  }
-  
 
-  move_latest_letter() {
-    let canvas = $("#canvas");
-    let letter = $("#canvas div:last-child");
-    let game = this;
-    letter.animate(
-      {
-        top: `${canvas.height() - letter.height()}px`
-      },
-      {
-        'duration': game.block_speed, 
-        "done": function () {
+    move_latest_letter() {
+        let canvas = $("#canvas");
+        let letter = $("#canvas div:last-child");
+        let game = this;
+        letter.animate(
+            {
+                top: `${canvas.height() - letter.height()}px`
+            },
+            {
+                'duration': game.block_speed,
+                "done": function () {
+                    // If this letter has already been cleared then just do nothing.
+                    if (!$(this).hasClass('cleared')) {
 
+                        game.show_wrong_answer_hint($(this).text());
+                        game.letters_on_screen--;
+                        game.blocks.shift();
 
-          // If this letter has already been cleared then just do nothing.
-          if (!$(this).hasClass('cleared')){
-            
-            game.show_wrong_answer_hint($(this).text());
-            game.letters_on_screen--;
-            game.blocks.shift();
-            
-            if(game.current_mode==='endless' && game.endless_points>0){
-              game.endless_points--;
-              game.update_state();
-            }else{
-              game._decrease_progress(letter.text());
+                        if (game.current_mode === 'endless' && game.endless_points > 0) {
+                            game.endless_points--;
+                            game.update_state();
+                        } else {
+                            game._decrease_progress(letter.text());
+                        }
+
+                    }
+                    $(this).remove();
+
+                },
+                'progress': function (animation, progress, msRemaining) {
+                    if (msRemaining < game.block_speed / 2 && !$(this).hasClass("red_text")) {
+                        $(this).addClass('red_text')
+                    }
+                },
+                "easing": "linear"
             }
-            
-          } 
-          $(this).remove();         
-          
-        },
-        'progress': function (animation, progress, msRemaining) {
-          if (msRemaining < game.block_speed/2 && !$(this).hasClass("red_text")) {
-            $(this).addClass('red_text')
-          }
-        },
-        "easing": "linear"
-      }
-
-    );
-  }
-
-  add_letter_to_canvas(letter) {
-    let new_letter_block = new LetterBlock(letter);
-    console.log(`${new_letter_block.letter}:${new_letter_block.reading}`);
-    this.blocks.push(new_letter_block);
-    this.last_block_added_time = Math.floor(Date.now() / 1000);
-    this.letters_on_screen++;
-
-    $('#canvas').append(`<div class='alive canvas_letter'>${letter}</div>`)
-
-    let new_letter = $("#canvas div:last-child");
-    let canvas_width = $("#canvas").width();
-    let letter_width = new_letter.width();
-
-    let minimum = 0;
-    let maximum = canvas_width - letter_width;
-    let pos = getRandomInt(minimum, maximum);
-    // Adjust letter css
-    new_letter.css("right", `${pos}px`);
-    new_letter.css("top", `-16px`);
-
-    this.move_latest_letter();
-
-  }
-
-
-  choose_new_block_endless() {
-    return random_choice(this.syllabus);
-
-  }
-
-  choose_new_block_exam() {
-    
-    // if all equally progressed then just choose one
-    let unique_values = new Set(Object.values(this.exam_progress));
-    if (unique_values.size===1){
-      return random_choice(Object.keys(this.exam_progress));
+        );
     }
 
-    // discard all that are not the lowest freq
-    let min_progress = Math.min(...Object.values(this.exam_progress));
-    let candidates = Object.entries(this.exam_progress).filter( ([key,value])=> value<=min_progress );
-    candidates = candidates.map(el => el[0]);
-    let choice = random_choice(candidates);
-    return choice;
-  }
+    add_letter_to_canvas(letter) {
+        let new_letter_block = new LetterBlock(letter);
+        console.log(`${new_letter_block.letter}:${new_letter_block.reading}`);
+        this.blocks.push(new_letter_block);
+        this.last_block_added_time = Math.floor(Date.now() / 1000);
+        this.letters_on_screen++;
 
+        $('#canvas').append(`<div class='alive canvas_letter'>${letter}</div>`)
 
-  // Game state
-  update_state() {
-    $("#points").text(`${this.endless_points}`);
-    $('#block_speed').text(`${this.block_speed/1000}s`);
-  }
+        let new_letter = $("#canvas div:last-child");
+        let canvas_width = $("#canvas").width();
+        let letter_width = new_letter.width();
 
-
-  main_loop() {
-    setInterval(() => {
-      if (this.current_mode === 'endless') {
-        this.endless_main_loop();
-      } else {
-        this.exam_main_loop();
-      }
-
-    }, 250);
-  }
-
-  endless_main_loop() {
-     // skip if paused.
-     if (!this.running){
-      return;
-    }
-    
-    // Skip if too many blocks on screen
-    if (letters_on_screen>=this.MAX_BLOCKS_ON_SCREEN){
-      return;
-    }
-    // Skip if block added too recently
-    if ( (Math.floor(Date.now() / 1000)-this.last_block_added_time) < this.NEW_BLOCK_DELAY ){
-      return;
+        let minimum = 0;
+        let maximum = canvas_width - letter_width;
+        let pos = getRandomInt(minimum, maximum);
+        // Adjust letter css
+        new_letter.css("right", `${pos}px`);
+        new_letter.css("top", `-16px`);
+        this.move_latest_letter();
     }
 
-    let letter = this.choose_new_block_endless();
-    this.add_letter_to_canvas(letter);
 
-  }
-
-  exam_main_loop() {
-
-    // skip if paused.
-    if (!this.running){
-      return;
+    choose_new_block_endless() {
+        return random_choice(this.syllabus);
     }
 
-    // Skip if too many blocks on screen
-    if (letters_on_screen>=this.MAX_BLOCKS_ON_SCREEN){
-      return;
+    choose_new_block_exam() {
+        // if all equally progressed then just choose one
+        let unique_values = new Set(Object.values(this.exam_progress));
+        if (unique_values.size === 1) {
+            return random_choice(Object.keys(this.exam_progress));
+        }
+
+        // discard all that are not the lowest freq
+        let min_progress = Math.min(...Object.values(this.exam_progress));
+        let candidates = Object.entries(this.exam_progress).filter(([key, value]) => value <= min_progress);
+        candidates = candidates.map(el => el[0]);
+        // pick one amongst the remaining candidates
+        return random_choice(candidates);
     }
-    // Skip if block added too recently
-    if ( (Math.floor(Date.now() / 1000)-this.last_block_added_time) < this.NEW_BLOCK_DELAY ){
-      return;
+
+
+    // Game state
+    update_state() {
+        $("#points").text(`${this.endless_points}`);
+        $('#block_speed').text(`${this.block_speed / 1000}s`);
     }
 
-    let letter = this.choose_new_block_exam();
-    this.add_letter_to_canvas(letter);
-    
-  }
 
-  start_game() {
-    if (!this.running) {
-      // this.reset_game();
-      this.running = true;
-      this.update_state();
+    main_loop() {
+        setInterval(() => {
+            if (this.current_mode === 'endless') {
+                this.endless_main_loop();
+            } else {
+                this.exam_main_loop();
+            }
+
+        }, 250);
     }
-  }
-  reset_game() {
-    this.running = false;
-    this.blocks_seen = 0;
-    this.endless_points = 0;
-    this.blocks = [];
-    this.block_speed = 1000;
-    this.round = 0;
-    this.last_blocked_added_in_round = 0;
-    this.update_state();
+
+    endless_main_loop() {
+        // skip if paused.
+        if (!this.running) {
+            return;
+        }
+
+        // Skip if too many blocks on screen
+        if (window.game.letters_on_screen >= this.MAX_BLOCKS_ON_SCREEN) {
+            return;
+        }
+        // Skip if block added too recently
+        if ((Math.floor(Date.now() / 1000) - this.last_block_added_time) < this.NEW_BLOCK_DELAY) {
+            return;
+        }
+
+        let letter = this.choose_new_block_endless();
+        this.add_letter_to_canvas(letter);
+
+    }
+
+    exam_main_loop() {
+        // skip if paused.
+        if (!this.running) {
+            return;
+        }
+
+        // Skip if too many blocks on screen
+        if (window.game.letters_on_screen >= this.MAX_BLOCKS_ON_SCREEN) {
+            return;
+        }
+        // Skip if block added too recently
+        if ((Math.floor(Date.now() / 1000) - this.last_block_added_time) < this.NEW_BLOCK_DELAY) {
+            return;
+        }
+
+        let letter = this.choose_new_block_exam();
+        this.add_letter_to_canvas(letter);
+    }
+
+    start_game() {
+        if (!this.running) {
+            // this.reset_game();
+            this.running = true;
+            this.update_state();
+        }
+    }
+
+    reset_game() {
+        this.running = false;
+        this.blocks_seen = 0;
+        this.endless_points = 0;
+        this.blocks = [];
+        this.block_speed = 1000;
+        this.round = 0;
+        this.last_blocked_added_in_round = 0;
+        this.update_state();
 
 
-  }
-  pause_game() {
-    this.running ? this.running = false : this.running = true;
-    this.running ? $("#pause").text("Pause game") : $("#pause").text("Resume game");
-    console.log(this.running);
-  }
+    }
 
-  _set_syllabus(syl){
-    this.syllabus=syl;
-  }
+    pause_game() {
+        this.running ? this.running = false : this.running = true;
+        this.running ? $("#pause_btn").text("Pause game") : $("#pause_btn").text("Resume game");
+        console.log(this.running);
+    }
 
-  _set_blockspeed(speed){
-    this.block_speed=speed;
-    this.update_state();
-  }
+    _set_syllabus(syl) {
+        this.syllabus = syl;
+    }
 
-  _reset_exam_progress(){
-    //Takes the current syllabus and sets the counter of how many correct
-    this.exam_progress={}
-    this.syllabus.forEach(letter=>{
-      this.exam_progress[letter]=0;
-    });
-  }
+    _set_blockspeed(speed) {
+        this.block_speed = speed;
+        this.update_state();
+    }
 
-  _increase_progress(letter){
-    if (this.exam_progress[letter]<3){
-      this.exam_progress[letter]++;
+    _reset_exam_progress() {
+        //Takes the current syllabus and sets the counter of how many correct
+        this.exam_progress = {}
+        this.syllabus.forEach(letter => {
+            this.exam_progress[letter] = 0;
+        });
+    }
 
-      let first_element = $(`#${this.current_exam} #${letter}`);
-      let first_failed = $(first_element).siblings().filter(':not(.passed)').first()
-      first_failed.removeClass('failed');
-      first_failed.addClass('passed');
-    
-    }    
-  }
+    _increase_progress(letter) {
 
-  _decrease_progress(letter){
-    if (this.exam_progress[letter]>0){
-      this.exam_progress[letter]--;
 
-      let first_element = $(`#${this.current_exam} #${letter}`);
-      let first_failed = $(first_element).siblings().filter(':not(.failed)').first()
-      first_failed.addClass('failed');
-      first_failed.removeClass('passed');
+        if (this.current_mode === 'exam') {
+            if (this.exam_progress[letter] < 3) {
+                this.exam_progress[letter]++;
 
-    }    
+                let first_element = $(`#${this.current_exam} #${letter}`);
+                let first_failed = $(first_element).siblings().filter(':not(.passed)').first()
+                first_failed.removeClass('failed');
+                first_failed.addClass('passed');
+            }
+        }else if(this.current_mode==='endless'){
 
-  }
+        }
+
+
+    }
+
+    _decrease_progress(letter) {
+        if (this.exam_progress[letter] > 0) {
+            this.exam_progress[letter]--;
+
+            let first_element = $(`#${this.current_exam} #${letter}`);
+            let first_failed = $(first_element).siblings().filter(':not(.failed)').first()
+            first_failed.addClass('failed');
+            first_failed.removeClass('passed');
+
+        }
+
+    }
 
 }
 
-module.exports = { Game };
+module.exports = {Game};
--- src/js/main.js
+++ src/js/main.js
@@ -1,62 +1,27 @@
-const { SYLLABUS_NAMES, SYLLABUS_DICT, RU_TO_LAT_FULL,RU_TO_LAT_HARD_ONLY, RUS_ALL_LETTERS_FULL,RUS_ALL_LETTERS_HARD_ONLY } = require('./alphabet.js');
-const { Game } = require('./classes/game.js');
-const { getRandomInt, random_choice, setCookie } = require("./classes/utility");
+const {
+    SYLLABUS_NAMES,
+    SYLLABUS_DICT,
+    RU_TO_LAT_FULL,
+    RU_TO_LAT_HARD_ONLY,
+    RUS_ALL_LETTERS_FULL,
+    RUS_ALL_LETTERS_HARD_ONLY
+} = require('./alphabet.js');
+const {Game} = require('./classes/game.js');
+const {getRandomInt, random_choice, setCookie} = require("./classes/utility");
 var $ = require("jquery");
 
 //DOM Manipulation
 
-function _start_exam(){
-    $("#exam_mode").addClass('active');
-    $("#endless_mode").removeClass('active');
-    $('#syllabus').hide();
-    $("#points").hide()
-    $("#choose_exam").hide('fast');
-    $('#exams').show('fast');
-
-    // clear data
-    window.game.blocks = [];
-    $('#canvas .canvas_letter').each( (index,letter) => {
-        $(letter).addClass('cleared');
-        $(letter).remove();
-    });
-     
-    window.game.current_mode = 'exam';
-}
-
-
-function start_exam(){
-
-}
-
-function start_exam_all_letters(){
-    _start_exam();
-    set_full_syllabus();
-    $('#alphabet_full').show('fast');
-    $('#alphabet_hard_only').hide('fast');
-    window.game._reset_exam_progress();
-}
-
-function start_exam_hard_only(){
-    _start_exam();
-    set_hard_only_syllabus();
-    $('#alphabet_full').hide('fast');
-    $('#alphabet_hard_only').show('fast');
-    window.game._reset_exam_progress();
-
-}
-
 // Event handlers
 function _correct_input(char, block) {
-
     try {
-        return block.reading.indexOf(char)!==-1        
+        return block.reading.indexOf(char) !== -1
     } catch {
         null;
     }
 }
 
 function evaluate_input(e) {
-
     // Only evaluate input on enter
     if (e.key === 'Enter' || e.key === 'NumpadEnter') {
         //do nothing if no blocks on screen, just clear input
@@ -64,23 +29,23 @@ function evaluate_input(e) {
             $("input#letter_input").val("");
         } else {
             let block = window.game.blocks[0];
-            let correct = _correct_input( $("input#letter_input").val()  , block);
+            let correct = _correct_input($("input#letter_input").val(), block);
             // clear the input
             $("input#letter_input").val("");
             //Get first letter div that does not have the cleared tag
             let letter = $('#canvas div:not(.cleared)').first();
 
             if (correct) {
-                if (window.game.current_mode==='exam'){
+                if (window.game.current_mode === 'exam') {
                     window.game._increase_progress(letter.text());
-                }else if (window.game.current_mode==='endless'){
+                } else if (window.game.current_mode === 'endless') {
                     window.game.endless_points++;
                 }
-                
+
             } else if (!correct) {
-                if (window.game.current_mode==='exam'){
+                if (window.game.current_mode === 'exam') {
                     window.game._decrease_progress(letter.text());
-                }else{
+                } else {
                     if (window.game.endless_points > 0) {
                         window.game.endless_points--;
                     }
@@ -90,31 +55,36 @@ function evaluate_input(e) {
 
             window.game.blocks.shift();
             letter.addClass('cleared');
-            if (window.game.letters_on_screen>=1){
+            if (window.game.letters_on_screen >= 1) {
                 window.game.letters_on_screen--;
             }
             // if there are no more letters on the screen set the delay so that a new one can be added at once
-            if (window.game.letters_on_screen===0){
-                window.game.last_block_added_time= (Math.floor(Date.now() / 1000))-1000;
+            if (window.game.letters_on_screen === 0) {
+                window.game.last_block_added_time = (Math.floor(Date.now() / 1000)) - 1000;
             }
-                window.game.update_state();
+            window.game.update_state();
         }
     }
 }
 
-function load_exams() {
-
-    Object.entries(SYLLABUS_DICT).forEach(([key,value])=>{
-        create_exam(`${key}`,value);
+function load_exams_and_syllabus() {
+    // Creates exams and syllabus buttons based on alphabet.js
+    Object.entries(SYLLABUS_DICT).forEach(([key, value]) => {
+        create_exam(`${key}`, value);
+        create_syllabus(key, value);
     });
+}
 
+function create_syllabus(exam_name, letters) {
+    let new_syllabus_btn = `<button class="choose_syllabus btn btn-primary" data-name="${exam_name}">${SYLLABUS_NAMES[exam_name]}</button>`
+    $("#syllabus_menu").append(new_syllabus_btn);
 }
 
 function create_exam(exam_name, letters) {
-
     //create button to select the exam
-    let new_exam_btn = `<button class="choose_exam btn btn-primary"  id="btn_${exam_name}" data-name="${SYLLABUS_NAMES[exam_name]}">${SYLLABUS_NAMES[exam_name]}</button>`;
-    $('#choose_exam').append(new_exam_btn);
+    let new_exam_btn = `<button class="choose_exam btn btn-primary"  id="btn_${exam_name}" data-name="${exam_name}">${SYLLABUS_NAMES[exam_name]}</button>`;
+
+    $('#choose_exam_menu').append(new_exam_btn);
 
     // create div to hold the exam
     let new_exam = `<div data-exam=${exam_name} id=${exam_name} class=exam></div>`;
@@ -132,101 +102,108 @@ function create_exam(exam_name, letters) {
     $(`#${exam_name}`).append(base);
 }
 
-function show_exams() {
-    $('#exams').slideToggle('slow');
-    $('#canvas_wrapper').toggleClass('hide');
-}
-
 function toggle_exam() {
+    let disp = $("#choose_exam_menu").css('display')
+    if (disp === "none") {
+        $("#choose_exam_menu").show('fast');
+    } else {
+        $("#choose_exam_menu").hide('fast');
+    }
 
-    $("#choose_exam").show('fast');
-    $("#exams").hide();
-    $('#syllabus').hide();
-    $("#points").hide()
-
-}
-
-function set_full_syllabus(){
-    //Selects what syllabus to use (what letters to pick form for challenge)
-    window.game._set_syllabus(RUS_ALL_LETTERS_FULL);
-    window.game.current_exam='alphabet_full';
-    $('#btn_alphabet_full').addClass('active');
-    $('#btn_alphabet_hard_only').removeClass('active');
-}
-
-function set_hard_only_syllabus(){
-    window.game._set_syllabus(RUS_ALL_LETTERS_HARD_ONLY);
-    window.game.current_exam='alphabet_hard_only';
-    $('#btn_alphabet_full').removeClass('active');
-    $('#btn_alphabet_hard_only').addClass('active');
 }
 
 function toggle_endless() {
-    $("#exam_mode").removeClass('active');
-    $("#endless_mode").addClass('active');
+    $("#exam_mode_btn").removeClass('active');
+    $("#endless_mode_btn").addClass('active');
     $("#exams").hide();
     $("#points").show();
     window.game.current_mode = 'endless';
 }
 
-function toggle_speed(){
+function toggle_speed() {
     $("#speed_menu").toggle('fast');
 }
 
-function toggle_syllabus(){
-    $("#syllabus").toggle('fast');
+function toggle_syllabus() {
+    $("#syllabus_menu").toggle('fast');
+}
+
+function toggle_challenge_mode(){
+    window.game.challenge_mode = !window.game.challenge_mode;
+    if (window.game.challenge_mode){
+        $("#challenge_mode_btn").addClass('active')
+    }else{
+        $("#challenge_mode_btn").removeClass('active')
+    }
 }
 
 function pause_game() {
     window.game.pause_game();
 }
 
+function toggle_show_hints() {
+    window.game.show_hints = !window.game.show_hints;
+}
+
 //add event listeners
-document.getElementById("show_exams").addEventListener('click', show_exams);
-document.getElementById('endless_mode').addEventListener('click', toggle_endless);
-document.getElementById('exam_mode').addEventListener('click', toggle_exam);
-document.getElementById('pause').addEventListener('click', pause_game);
-
-document.getElementById('btn_alphabet_full').addEventListener('click',set_full_syllabus);
-document.getElementById('btn_alphabet_hard_only').addEventListener('click',set_hard_only_syllabus);
-
+document.getElementById('endless_mode_btn').addEventListener('click', toggle_endless);
+document.getElementById('exam_mode_btn').addEventListener('click', toggle_exam);
+document.getElementById('pause_btn').addEventListener('click', pause_game);
 
 document.getElementById('syllabus_btn').addEventListener('click', toggle_syllabus);
 document.getElementById("letter_input").addEventListener('keydown', evaluate_input);
 
-document.getElementById("speed_btn").addEventListener('click',toggle_speed);
+document.getElementById("speed_btn").addEventListener('click', toggle_speed);
 
-document.getElementById("show_hints").addEventListener('click',show_hints)
+document.getElementById('challenge_mode_btn').addEventListener('click',toggle_challenge_mode)
 
-$('.speed_btn').on('click',(e)=>{
+$('.speed_btn').on('click', (e) => {
     let speed = $(e.target).data('speed');
     window.game._set_blockspeed(speed);
     $('#speed_menu').hide('fast');
 });
 
-$('#speed_custom').on('click',(e)=>{
-let speed = window.prompt('Enter how many seconds you have to answer');
-if (speed !==null && speed!=="" && parseFloat(speed)>=0.1);
-    window.game._set_blockspeed(parseFloat(speed)*1000);
+
+$('#speed_custom').on('click', (e) => {
+    let speed = window.prompt('Enter how many seconds you have to answer');
+    if (speed !== null && speed !== "" && parseFloat(speed) >= 0.1) ;
+    window.game._set_blockspeed(parseFloat(speed) * 1000);
     $("#speed_menu").hide('fast');
 });
 
-$('#show_hints').on('change',(e)=>{
-    if ($("#show_hints").is(':checked')){
-        window.game.show_hints=true;
-    }else{
-        window.game.show_hints=false;
-    }
-
-    console.log(this.show_hints);
+$('#show_hints').on('change', (e) => {
+    toggle_show_hints()
 })
 
 
 $(document).ready(function () {
-    load_exams();
-    letters_on_screen = 0;
-    canvas = $("#canvas");
+    // Load exams
+    load_exams_and_syllabus();
+
+    // event listeners for dynamically created buttons
+    $(".choose_syllabus").on('click', (e) => {
+        let syl_name = $(e.target).data('name');
+        let syl = SYLLABUS_DICT[syl_name];
+        window.game._set_syllabus(syl);
+        $('#syllabus_menu').hide('fast');
+    });
+
+    $('.choose_exam').on('click',(e)=>{
+        let exam_name = $(e.target).data('name');
+        let syl = SYLLABUS_DICT[exam_name];
+        window.game._set_syllabus(syl);
+
+        $(`#${exam_name}`).css('display','flex');
+
+        $("#exam_mode_btn").addClass('active');
+        $("#endless_mode_btn").removeClass('active');
+        $("#choose_exam_menu").hide('fast');
+
+    })
+
+    // create game object in the global scope
     window.game = new Game();
     window.game.start_game();
+    //start the main loop
     window.game.main_loop();
 });
